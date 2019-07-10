f44be8aab477b89b4a1fc1f9947665624b49fa3d
johnson_phillips <johnson.phillips@mheducation.com>
2019-07-10 14:03:15 -0400

    added sample testcase

--- src/test/java/qa/automation/tests/ApiTest.java
+++ src/test/java/qa/automation/tests/ApiTest.java
@@ -2,7 +2,6 @@ package qa.automation.tests;
 
 import org.testng.annotations.Test;
 import qa.automation.core.Api;
-
 import static qa.automation.report.TestData.*;
 import static qa.automation.core.Api.*;
 
@@ -11,12 +10,9 @@ public class ApiTest {
     @Test
     public void Test1() throws Exception
     {
+        startTest("sample post request",1);
         Api.printconsole = true;
-        startTest("first test",1);
-        for(int i=0;i<5;i++) {
-            readResponse(given().body("{\"project_id\":\"1\"}").post("http://localhost:8080/qeportal/OutputApi/getProducts"));
-            readResponse(given().get("http://localhost:8080/qeportal/OutputApi/getTopics"));
-        }
+        readResponse(given().body("{\"name\":\"afbffg\",\"salary\":\"123\",\"age\":\"23\"}").post("http://dummy.restapiexample.com/api/v1/create"));
         endTest();
     }
 }
